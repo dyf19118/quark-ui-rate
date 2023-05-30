8802d6c089f0eaea57803a63ba5f08c2c0252645
duyifei121 <duyifei121@hellobike.com>
2023-05-30 16:45:14 +0800

    feat: add type definition & remove useless files and deps

--- src/main.tsx
+++ src/main.tsx
@@ -7,25 +7,25 @@ const cssValueRE = /^((?:\d+)?(?:\.\d*)?)([a-zA-Z%]+)?$/;
 
 @customElement({ tag: "quark-ui-rate", style })
 class QuarkUiRate extends QuarkElement {
-  /** 当前分值 */
+  /** current rating value */
   @property({ type: Number })
   value = 0;
-  /** 图标大小——默认单位px */
+  /** size of icon —— unit default to px */
   @property({ type: String })
   size: string = "20";
-  /** 图标间距——默认单位px */
+  /** space between icons —— unit default to px */
   @property({ type: String })
   space: string = "4";
-  /** 总分——星星数量 */
+  /** icon count —— top rating value */
   @property({ type: Number })
   count = 5;
-  /** 选中时颜色 */
+  /** active color */
   @property({ type: String })
   activeColor = "linear-gradient(to right, #FB990F, #FF5526)";
-  /** 未选中时颜色 */
+  /** default color */
   @property({ type: String })
   color = "#F0F3F5";
-  /** 图标地址 */
+  /** icon's url */
   @property({ type: String })
   icon = defaultIconURL;
   /** is half rate allowed */
@@ -38,12 +38,12 @@ class QuarkUiRate extends QuarkElement {
   @property({ type: Boolean })
   disabled = false;
 
-  get roundedValue() {
+  private get roundedValue() {
     return Math.min(this.count, Math.max(0, this.value));
   }
 
-  /** 解析CSS值——如1px、1rem */
-  _parseValue(cssValue: string, defaultUnit = "px"): [number, string] {
+  /** parse css values —— eg. 1px、1rem */
+  private _parseValue(cssValue: string, defaultUnit = "px"): [number, string] {
     const match = cssValue.match(cssValueRE);
 
     if (!match) {
@@ -54,57 +54,56 @@ class QuarkUiRate extends QuarkElement {
     return [parseFloat(value), unit || defaultUnit];
   }
 
-  parseValue = memoize(
+  private parseValue = memoize(
     this._parseValue
     // (...args) => JSON.stringify(args),
   );
 
-  formatParsedValue([value, unit]: [number, string]) {
+  private formatParsedValue([value, unit]: [number, string]) {
     return `${value}${unit}`;
   }
 
-  get parsedSize() {
+  private get parsedSize() {
     return this.parseValue(this.size);
   }
 
-  get parsedSizeValue() {
+  private get parsedSizeValue() {
     return this.parsedSize[0];
   }
 
-  get parsedSizeUnit() {
+  private get parsedSizeUnit() {
     return this.parsedSize[1];
   }
 
-  get formattedSize() {
+  private get formattedSize() {
     return this.formatParsedValue(this.parsedSize);
   }
 
-  /** 进度，0-1之间的小数 */
-  get progress() {
+  /** progress，decimal between 0-1 */
+  private get progress() {
     return this.roundedValue / this.count;
   }
 
-  /** 进度导轨的背景色宽度——也就是原始宽度 */
-  get progressBgWidth() {
+  /** background width of progress track —— original width */
+  private get progressBgWidth() {
     return this.parsedSizeValue * this.count;
   }
 
-  /** 进度导轨的真实宽度 */
-  get progressWidth() {
+  /** track's real with */
+  private get progressWidth() {
     return this.progressBgWidth * this.progress;
   }
 
-  _getClassNames(classNameMap: Record<string, boolean>) {
+  private _getClassNames(classNameMap: Record<string, boolean>) {
     return Object.entries(classNameMap)
       .filter(([_, value]) => value)
       .map(([className]) => className)
       .join(" ");
   }
 
-  getClassNames = memoize(this._getClassNames);
+  private getClassNames = memoize(this._getClassNames);
 
   componentDidUpdate(propName: string, oldValue: string, newValue: string) {
-    // 已更新
     if (propName === "value") {
       this.$emit("change", {
         detail: newValue,
@@ -116,7 +115,7 @@ class QuarkUiRate extends QuarkElement {
     this.parseValue.cache.clear();
   }
 
-  async handleClick(e: PointerEvent, index: number) {
+  private async handleClick(e: PointerEvent, index: number) {
     if (!(e.target instanceof HTMLElement) || this.readonly || this.disabled) {
       return;
     }
@@ -183,3 +182,9 @@ class QuarkUiRate extends QuarkElement {
 }
 
 export default QuarkUiRate;
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "quark-ui-rate": QuarkUiRate;
+  }
+}
