c80adbe31610d70d86edf1cfd3ec04d10466f278
duyifei121 <duyifei121@hellobike.com>
2023-06-06 19:58:38 +0800

    fix: declaraction file generation

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
+import typescript from '@rollup/plugin-typescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,9 @@ export default defineConfig({
       output: {
         dir: "lib",
       },
+      plugins: [
+        typescript()
+      ],
     },
   },
 });
